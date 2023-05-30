557852e0c11ad4018f1ff6e0c7b427e00516c62b
Marc-Andre Pepin <1marc.andre.pepin@gmail.com>
2023-05-30 12:32:47 -0500

    Commiting all changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'homepage#index'
 
   # Add routes below this line
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
 
   # Add routes below above line
+  delete 'tasks/:id' => 'tasks#destroy'
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put '/tasks/:id/mark_active'   => 'tasks#mark_active'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
